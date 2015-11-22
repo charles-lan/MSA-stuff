3260d57e799e19325b5f0d35035608c3bcc32cc7
Charles Lan <charlis.lan@gmail.com>
2015-11-23 12:25:24 +1300

    Stuff

---
+++ SignalRChat/SignalRChat/Startup1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(SignalRChat.Startup1))]
+
+namespace SignalRChat
+{
+    public class Startup1
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // Any connection or hub wire up and configuration should go here
+            app.MapSignalR();
+            // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=316888
+        }
+    }
+}
